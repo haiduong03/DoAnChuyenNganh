983157a6564cd3ac8466390614934350ed77c84e
haiduong <haiduong03102k@gmail.com>
2021-12-29 22:57:17 +0700

    sua product

--- controller/productcontroller.php
+++ controller/productcontroller.php
@@ -50,6 +50,148 @@ function deleteProduct($id)
     header("Location: ../product.php?Message=Delete successfull !!!");
 }
 
+function changeName($product)
+{
+    $new = $product->get_name();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductName='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function changeDetail($product)
+{
+    $new = $product->get_detail();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductDetail='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../admin.php?Message=Change successfull !!!");
+}
+
+function changeType($product)
+{
+    $new = $product->get_type();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductType='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function changeBrand($product)
+{
+    $new = $product->get_brand();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductBrand='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../admin.php?Message=Change successfull !!!");
+}
+
+function changeSize($product)
+{
+    $new = $product->get_size();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductSize='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function changeColor($product)
+{
+    $new = $product->get_color();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductColor='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function changeImage($product)
+{
+    $new = $product->get_image();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductImg=$new WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function changePrice($product)
+{
+    $new = $product->get_price();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductPrice='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function changePromotion($product)
+{
+    $new = $product->get_promotion();
+    $id = $product->get_id();
+
+    $stmt = connectDatabase()->prepare("UPDATE PRODUCT SET ProductPromotion='$new' WHERE ProductID = '$id'");
+    $stmt->execute();
+    header("Location: ../product.php?Message=Change successfull !!!");
+}
+
+function printProduct()
+{
+    include_once './model/product.php';
+
+    $stmt = connectDatabase()->prepare("SELECT * FROM PRODUCT ");
+    $stmt->execute();
+    $listProduct = array();
+    while ($row = $stmt->fetch()) {
+        array_push(
+            $listProduct,
+            new Product(
+                $row['ProductID'],
+                $row['ProductName'],
+                $row['ProductDetail'],
+                $row['ProductBrand'],
+                $row['ProductType'],
+                $row['ProductSize'],
+                $row['ProductColor'],
+                $row['ProductImg'],
+                $row['ProductPrice'],
+                $row['ProductPromotion']
+            )
+        );
+    }
+
+    return $listProduct;
+}
+
+function inforProduct($id)
+{
+    include_once './model/product.php';
+
+    $stmt = connectDatabase()->prepare("SELECT * FROM PRODUCT WHERE ProductID='$id';");
+    $stmt->execute();
+    $product = $stmt->fetchAll();
+    foreach ($product as $row) {
+        $product = new Product(
+            $row['ProductID'],
+            $row['ProductName'],
+            $row['ProductDetail'],
+            $row['ProductBrand'],
+            $row['ProductType'],
+            $row['ProductSize'],
+            $row['ProductColor'],
+            $row['ProductImg'],
+            $row['ProductPrice'],
+            $row['ProductPromotion']
+        );
+    }
+    return $product;
+}
+
 if (isset($_POST['product'])) {
     switch ($_POST['product']) {
         case $_POST['product'] == 'create':
@@ -79,6 +221,63 @@ if (isset($_POST['product'])) {
             $id = $_GET['id'];
 
             deleteProduct($id);
+
+            break;
+        case $_POST['product'] == 'change':
+
+            $id = $_GET['id'];
+            $name = $_POST['name'];
+            $detail = $_POST['detail'];
+            $brand = $_POST['brand'];
+            $type = $_POST['type'];
+            $size = $_POST['size'];
+            $color = $_POST['color'];
+            if ($_FILES['image']['name'] > 0)
+                $image = file_get_contents($_FILES['image']['tmp_name']);
+            else
+                $image = null;
+            $price = $_POST['price'];
+            $promotion = $_POST['promotion'];
+
+
+            include "../model/product.php";
+            $product = new Product($id, $name, $detail, $brand, $type, $size, $color, $image, $price, $promotion);
+
+            if ($product->get_name() > 0) {
+                changeName($product);
+            }
+
+            if ($product->get_detail() > 0) {
+                changeDetail($product);
+            }
+
+            if ($product->get_brand() > 0) {
+                changeBrand($product);
+            }
+
+            if ($product->get_type() > 0) {
+                changeType($product);
+            }
+
+            if ($product->get_size() > 0) {
+                changeSize($product);
+            }
+
+            if ($product->get_color() > 0) {
+                changeColor($product);
+            }
+
+            if ($product->get_image() != 0) {
+                changeImage($product);
+            }
+
+            if ($product->get_price() > 0) {
+                changePrice($product);
+            }
+
+            if ($product->get_promotion() >= 0) {
+                changePromotion($product);
+            }
 
             break;
     }
